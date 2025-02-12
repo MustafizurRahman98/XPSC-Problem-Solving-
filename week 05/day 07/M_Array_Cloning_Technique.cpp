2041aaa4865fd5cb7f663fc9de13df680c760801
Mustafizur Rahman <155304486+MustafizurRahman98@users.noreply.github.com>
2025-02-13 00:12:45 +0600

    Add files via upload

--- week 05/day 07/M_Array_Cloning_Technique.cpp
+++ week 05/day 07/M_Array_Cloning_Technique.cpp
@@ -7,7 +7,40 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        map<ll, ll> frq;
+        for (ll val : a)
+        {
+            frq[val]++;
+        }
+
+        ll mxVal = 0;
+        for (ll v : a)
+        {
+            mxVal = max(mxVal, frq[v]);
+        }
+
+        ll other = n - mxVal;
+        ll ans = 0;
+        while (mxVal < n)
+        {
+            mxVal *= 2;
+            ans++;
+        }
+
+        cout << ans + other << endl;
+    }
 
     return 0;
 }
