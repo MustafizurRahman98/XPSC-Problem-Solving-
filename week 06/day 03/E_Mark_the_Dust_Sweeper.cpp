569eddf58377dc860e89cac8a619d1253dca1630
Mustafizur Rahman <155304486+MustafizurRahman98@users.noreply.github.com>
2025-02-25 22:35:52 +0600

    Add files via upload

--- week 06/day 03/E_Mark_the_Dust_Sweeper.cpp
+++ week 06/day 03/E_Mark_the_Dust_Sweeper.cpp
@@ -7,7 +7,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        ll x = 0;
+        for (ll i = 0; i < n - 1; i++)
+        {
+            if (a[i] == 0)
+            {
+                if (x > 0)
+                {
+                    x++;
+                }
+            }
+            else
+            {
+                x += a[i];
+            }
+        }
+        cout << x << endl;
+    }
 
     return 0;
 }
--- week 06/day 03/F_Good_Prefixes.cpp
+++ week 06/day 03/F_Good_Prefixes.cpp
@@ -7,7 +7,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        ll sum = 0, ans = 0, mx = LLONG_MIN;
+        for (ll i = 0; i < n; i++)
+        {
+            sum += a[i];
+            mx = max(mx, a[i]);
+            if (sum % 2 == 0)
+            {
+                if ((sum - mx) == mx)
+                {
+                    ans++;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
 
     return 0;
 }
