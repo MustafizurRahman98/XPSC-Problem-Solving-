529e7eb9bba07fd9d8fe125adc265b05558c8872
Mustafizur Rahman <155304486+MustafizurRahman98@users.noreply.github.com>
2025-03-12 22:22:46 +0600

    Add files via upload

--- week 08/day 01/B_Maximise_Score.cpp
+++ week 08/day 01/B_Maximise_Score.cpp
@@ -7,7 +7,38 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        int scor = INT_MAX;
+
+        for (int i = 0; i < n; i++)
+        {
+            int bobScr = 0;
+            if (i > 0)
+            {
+                bobScr = max(bobScr, abs(a[i] - a[i - 1]));
+            }
+            if (i < n - 1)
+            {
+                bobScr = max(bobScr, abs(a[i] - a[i + 1]));
+            }
+
+            scor = min(scor, bobScr);
+           
+        }
+
+        cout << scor << endl;
+    }
 
     return 0;
 }
